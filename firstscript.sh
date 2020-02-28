252dda9d53bab10a0d25e2a3e7a79bad58ccbd4f
killianbuton <killian.butonsum@gmail.com>
2020-02-28 13:08:05 +0100

    1premier script

---
+++ firstscript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+
+
+
+
+echo "hello word"
+
+for fich in /tmp/in/*
+do
+        if [ -f $fich ]
+        then
+               gzip $fich
+
+
+       mv $fich/tmp/out/*
+        fi
+
+
+
+done
